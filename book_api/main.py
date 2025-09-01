a434f3f1ceaaf9a96780281ac0bc20b384bde39d
Vlad-Cristian Brinzas <Vlad-Cristian.Brinzas@endava.com>
2025-09-01 15:39:19 +0300

    Set up lifespan block for ensuring DB is up before API runs

--- book_api/main.py
+++ book_api/main.py
@@ -2,16 +2,31 @@
 # We do need to figure out what we want to return
 # from these endpoints (how much detail, what format)
 # and get the service to do that.
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from pydantic import BaseModel
 from book_api.open_ai_service import get_response_text
+from book_api.persistence import setup_database
 
 
 class PromptRequest(BaseModel):
     prompt: str
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    # Setup persistence (SQLite DB)
+    setup_database()
+
+    yield
+
+    # Shutdown
+    # (Nothing to do here currently)
+    # (Could consider closing connections if needed)
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/health")
